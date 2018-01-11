cbab9f7e298c7f6cb6ead4146d8f20cddff209a0
lucassid <lucassidpacheco@gmail.com>
2018-01-11 08:11:30 -0300

    plot generator for ssim

---
+++ ssimplot.py
@@ -0,0 +1,27 @@
+import sys
+import matplotlib.pyplot as plt, numpy as np
+
+if (len(sys.argv) != 2):
+    print ("passar o arquivo como parâmetro")
+    sys.exit(0)
+
+value = []
+with open (sys.argv[1]) as f:
+    content = f.readlines()
+for i in content:
+    try:
+        float(i)
+        value.append(i)
+        print (i)
+    except ValueError:
+        pass
+value = np.array(value)
+x = np.arange(0, 10, 10/len(value))
+#y = np.arange(0, 1, 1/len(value))
+plt.xlim(0.0, 10)
+#plt.ylim(0.0, 1)
+plt.yticks(np.arange(0, 1, 0.1))
+#plt.xticks(np.arange(0, 10, 1/len(value)))
+plt.plot(x, value)
+
+plt.show()
