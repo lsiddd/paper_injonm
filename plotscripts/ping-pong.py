909aa70e2a921040c5e764077bd0976db835efcd
lucassid <lucassidpacheco@gmail.com>
2018-02-27 11:05:37 -0300

    ping pong function corrected

--- plotscripts/ping-pong.py
+++ plotscripts/ping-pong.py
@@ -2,57 +2,93 @@
 
 import re, os, sys
 
-handover = []
-pp = set()
-verbose = 0
-checked = []
-if (len(sys.argv) > 1):
-	filename = sys.argv[1]
-	#--------------CONTRUÇÃO DA MATRIZ COM INFOS DE HANDOVER--------------
-	if (os.path.isfile(filename)): #LEITURA DO ARQUIVO DE RUN
-		with open(filename, 'r') as f:
-			data = f.readlines()
-			for i in data: #REGEX PARA EXTRAIR INFOS
-				if (re.search("doing handover", i)):
+verbose = False
+plot = False
+interval = 6
 
-					simtime = re.search("^[-+]?([0-9]*\.[0-9]+|[0-9]+)", i)
+def parseRun(filename):
+		global verbose
+		handover = []
+		#--------------CONTRUÇÃO DA MATRIZ COM INFOS DE HANDOVER--------------
+		if (os.path.isfile(filename)): #LEITURA DO ARQUIVO DE RUN
+			with open(filename, 'r') as f:
+				data = f.readlines()
+				for i in data: #REGEX PARA EXTRAIR INFOS
+					if (re.search("doing handover", i)):
 
-					imsi = re.search("IMSI ([0-9][0-9]|[0-9])", i)
-					imsi = re.search("([0-9][0-9]|[0-9])", imsi.group(0))
+						simtime = re.search("^[-+]?([0-9]*\.[0-9]+|[0-9]+)", i)
 
-					originCell = re.search("CellId ([0-9][0-9]|[0-9])", i)
-					originCell = re.search("([0-9][0-9]|[0-9])", originCell.group(0))
+						imsi = re.search("IMSI ([0-9][0-9]|[0-9])", i)
+						imsi = re.search("([0-9][0-9]|[0-9])", imsi.group(0))
 
-					targetCell = re.search("([0-9][0-9]|[0-9])$", i)
+						originCell = re.search("CellId ([0-9][0-9]|[0-9])", i)
+						originCell = re.search("([0-9][0-9]|[0-9])", originCell.group(0))
 
-					if (verbose):
-						print (i)
-						print ("tempo do handover:\t" + str(simtime.group(0)))
-						print("imsi do nó:\t" + str(imsi.group(0)))
-						print("célula origem:\t" + str(originCell.group(0)))
-						print("célula alvo:\t" + str(targetCell.group(0)) + "\n\n\n")
+						targetCell = re.search("([0-9][0-9]|[0-9])$", i)
 
-					handover.append([simtime.group(0), imsi.group(0), originCell.group(0), targetCell.group(0)])
-		print (handover)
-	else:
-		print ("isto non exsiste")
-	#------------------------------------------------------------------------
-'''
-	#-------------------------DETECÇÃO DE PING-PONG--------------------------
+						if (verbose):
+							print (i)
+							print ("tempo do handover:\t" + str(simtime.group(0)))
+							print("imsi do nó:\t" + str(imsi.group(0)))
+							print("célula origem:\t" + str(originCell.group(0)))
+							print("célula alvo:\t" + str(targetCell.group(0)) + "\n\n\n")
+
+						handover.append([float(simtime.group(0)), 
+										int(imsi.group(0)), 
+										int(originCell.group(0)), 
+										int(targetCell.group(0))
+										])
+		else:
+			print ("isto non exsiste")
+
+		return handover
+
+def findPP(handover):
+	checked = []
+	global interval
+	pp = 0
 	for i in handover:
-		origin = i[2]
-		for u in handover:
-			if (i[1] == u[1]):
-				if (float(u[0]) - float(i[0]) < 6 and
-					float(u[0]) - float(i[0]) > 0 and
-					origin == u[3]):
-					if i[1] not in checked:
-						pp = pp + 1
-				origin = u[2]
-		checked.append(i[1])
+		if i[1] not in checked:
+			cellList = []
+			cellList.append([i[2], 0])
+			for k in handover:
+				if (k[1] == i[1]):
+					cellList.append([k[3], k[0]])
+			checked.append(i[1])
 
-	#------------------------------------------------------------------------
-else:
-	print ("passar o nome do arquivo como parâmetro")
-print("realizados " + str(pp) + " handover ping pong")
-'''
+			for i in range(len(cellList) - 2):
+				if (cellList[i+2][0] == cellList[i][0] and cellList[i+2][1] - cellList[i][1] < interval):
+					pp = pp +1
+	print (pp)
+
+	return pp
+
+def test():
+	#if the calculation is correct, findPP should return 2 for this matrix
+	testMatrix=[[18.0 , 6, 11,  1], 
+				[18.32, 3, 1 , 11],
+				[18.35, 5, 2 ,  3], 
+				[18.4 , 6, 1 , 11], 
+				[19.32, 3, 11,  1],
+				[19.35, 5, 3 ,  2], 
+				[21.0 , 6, 11,  1], 
+				[21.08, 3, 1 , 11],
+				[30.0 , 5, 2 ,  3]]
+
+	if (findPP(testMatrix) != 2):
+		raise ValueError("k' something's vry wrong here")
+
+def main():
+	global testEnabled
+
+	if (len(sys.argv) == 1):
+		raise Exception("Passar arquivo de run como parâmetro")
+	
+	if(sys.argv[1] == "--test"):
+		test()
+
+	else:
+		findPP(parseRun(sys.argv[1]))
+
+if (__name__ == "__main__"):
+	main()
